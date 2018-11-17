9155c3ca6334cb49a8112ddc656655ee91cd05f1
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2018-11-17 19:23:04 -0200

    API context Path

--- src/main/java/com/marcosstefani/weatherdb/WeatherdbApplication.java
+++ src/main/java/com/marcosstefani/weatherdb/WeatherdbApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherdbApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/api/v1");
 		SpringApplication.run(WeatherdbApplication.class, args);
 	}
 }

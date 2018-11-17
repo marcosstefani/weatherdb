34144bf9be8b227cde8df401e41e0f17d7f2e7cc
Marcos Stefani Rosa <marcos.cantor@gmail.com>
2018-11-17 12:58:35 -0200

    first commit

---
+++ src/main/java/com/marcosstefani/weatherdb/WeatherdbApplication.java
@@ -0,0 +1,12 @@
+package com.marcosstefani.weatherdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherdbApplication.class, args);
+	}
+}
